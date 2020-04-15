53a3790394256ccd6178ac4908b3822d1d54d697
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-15 16:47:26 +0900

    emailの一意性を強制するマイグレーション
    $ rails g migration add_index_to_users_email
    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_053138) do
+ActiveRecord::Schema.define(version: 2020_04_15_074419) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_053138) do
     t.string "string"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
