244a4b1e28fe4db9be4cd832277ddf37fcc15680
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-15 15:28:30 +0900

    spec内でfactory_botで@userを初期化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    @user = build(:user)
+  end
+
   it 'should be valid' do
-    user = User.new(name: 'example user', email: 'user@example.com')
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end
 end
