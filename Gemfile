9123e4f1b046a1e471a97b4edc01d6dcaa9f6a97
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-28 18:54:03 +0900

    rspec-request_describerの導入
    $ bundle install

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 4.0.0'
+  gem 'rspec-request_describer'
 end
 
 group :development do
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
 
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/signup'
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /signup' do
+    it { is_expected.to eq(200) }
   end
 
 end
