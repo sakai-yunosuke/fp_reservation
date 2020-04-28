343054c65c4a5fb5d28150378368e0c050216aae
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-28 10:47:16 +0900

    shoulda_matchersの導入
    $ bundle install

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,83 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) do
-    @user = build(:user)
-  end
+  let(:user){ build(:user) }
 
-  it 'should be valid' do
-    expect(@user).to be_valid
-  end
+  describe 'validations' do
+    # subject { build(:user) }
 
-  it 'name should be present' do
-    @user.name = ''
-    expect(@user).to_not be_valid
-  end
-
-  it 'email should be present' do
-    @user.email = ''
-    expect(@user).to_not be_valid
-  end
-
-  it 'name should not be too long' do
-    @user.name = 'a' * 51
-    expect(@user).to_not be_valid
-  end
-
-  it 'email should not be too long' do
-    @user.name = 'a' * 244 + '@example.com'
-    expect(@user).to_not be_valid
-  end
-
-  it 'email validation should accept valid addresses' do
-    valid_addresses = %w[
-      user@example.com
-      USER@foo.COM
-      A_US-ER@foo.bar.org
-      first.last@foo.jp
-      alice+bob@baz.cn
-    ]
-    valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      expect(@user).to be_valid
-    end
-  end
-
-  it 'email validation should reject invalid addresses' do
-    invalid_addresses = %w[
-      user@example,com
-      user_at_foo.org
-      user.name@example.
-      foo@bar_baz.com
-      foo@bar+baz.com
-    ]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      expect(@user).to_not be_valid
-    end
-  end
-
-  it 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
-    expect(duplicate_user).to_not be_valid
-  end
-
-  it 'email addresses should be saved as lower-case' do
-    mixed_case_email = 'Foo@ExAmpLe.CoM'
-    @user.email = mixed_case_email
-    @user.save
-    expect(mixed_case_email.downcase).to eq(@user.reload.email)
-  end
-
-  it 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = ' ' * 8
-    expect(@user).to_not be_valid
-  end
-
-  it 'password should have a minimum length' do
-    @user.password = @user.password_confirmation = 'a' * 7
-    expect(@user).to_not be_valid
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
   end
 end
