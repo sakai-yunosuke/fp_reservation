57e2481556fbb3adf956517ed844ca33e0e547b8
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-30 10:39:14 +0900

    UsersControllerのspecであることを明示

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe UsersController, type: :request do
 
   describe 'GET /signup' do
     it { is_expected.to eq(200) }
