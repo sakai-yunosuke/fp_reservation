9a90f6cc398ea8f97a1990b6de4f9e4ba4164c8c
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-04-30 15:53:34 +0900

    ユーザー登録のテスト追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,4 +6,42 @@ RSpec.describe UsersController, type: :request do
     it { is_expected.to eq(200) }
   end
 
+  describe 'POST /signup' do
+    context 'when signup information is valid' do
+      it 'should be successful request' do
+        post signup_path, params: { user: FactoryBot.attributes_for(:user) }
+        expect(response.status).to eq 302
+      end
+
+      it 'should be success signup' do
+        expect do
+          post signup_path, params: { user: FactoryBot.attributes_for(:user) }
+        end.to change(User, :count).by(1)
+      end
+
+      it 'should redirect to user page' do
+        post signup_path, params: { user: FactoryBot.attributes_for(:user) }
+        expect(response).to redirect_to User.last
+      end
+    end
+
+    context 'when signup information is invalid' do
+      it 'should be successful request' do
+        post signup_path, params: { user: FactoryBot.attributes_for(:user, :invalid) }
+        expect(response.status).to eq 200
+      end
+
+      it 'should be fail signup' do
+        expect do
+          post signup_path, params: { user: FactoryBot.attributes_for(:user, :invalid) }
+        end.to_not change(User, :count)
+      end
+
+      it 'should show error message' do
+        post signup_path, params: { user: FactoryBot.attributes_for(:user, :invalid) }
+        expect(response.body).to include 'errors'
+      end
+    end
+  end
+
 end
