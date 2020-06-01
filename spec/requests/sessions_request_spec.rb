ada3f327f841237188da0126175d0807717cfebb
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-06-01 17:30:37 +0900

    ログイン失敗時のspecを追加

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
+RSpec.describe SessionsController, type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/sessions/new"
-      expect(response).to have_http_status(:success)
+  describe 'GET /login' do
+    it { is_expected.to eq(200) }
+  end
+
+  describe 'POST /login' do
+    context 'with invalid login information' do
+      before do
+        params[:session] = { email: '', password: '' }
+      end
+
+      it 'should fail singup and show error message' do
+        is_expected.to eq 200
+        expect(flash[:danger]).to eq 'Invalid email or password combination'
+      end
     end
   end
 
