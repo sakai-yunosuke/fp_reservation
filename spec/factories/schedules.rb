168119b320f42153140ee85a7c112a0876ad78b4
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-07-06 15:49:29 +0900

    Scheduleモデルのspecに使用するFactoryBotにて固定された日付を使用していたため修正

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,34 +1,39 @@
 FactoryBot.define do
   factory :schedule do
     user { FactoryBot.build(:user) }
-    start_time { DateTime.new(2020, 7, 1, 10, 30, 0) }
+
+    now = Time.current
+    start_time { now }
+
+    monday_in_business_hour = now.beginning_of_week(:monday).since(1.week).change(hour: 12)
+    saturday_in_business_hour = now.beginning_of_week(:saturday).since(1.week).change(hour: 12)
 
     trait :in_the_past do
-      start_time { DateTime.new(2000, 1, 1, 0, 0, 0) }
+      start_time { now.change(year: 2000) }
     end
 
     trait :not_every_half_hour do
-      start_time { DateTime.new(2020, 7, 1, 12, 15, 0) }
+      start_time { monday_in_business_hour.change(min: 15) }
     end
 
     trait :too_early_on_weekday do
-      start_time { DateTime.new(2020, 7, 1, 9, 0, 0) }
+      start_time { monday_in_business_hour.change(hour: 9) }
     end
 
     trait :too_late_on_weekday do
-      start_time { DateTime.new(2020, 7, 1, 18, 0, 0) }
+      start_time { monday_in_business_hour.change(hour: 18) }
     end
 
     trait :too_early_on_saturday do
-      start_time { DateTime.new(2020, 7, 4, 10, 0, 0) }
+      start_time { saturday_in_business_hour.change(hour: 10) }
     end
 
     trait :too_late_on_saturday do
-      start_time { DateTime.new(2020, 7, 4, 15, 0, 0) }
+      start_time { saturday_in_business_hour.change(hour: 15) }
     end
 
     trait :on_sunday do
-      start_time { DateTime.new(2020, 7, 5, 12, 0, 0) }
+      start_time { saturday_in_business_hour + 1.days }
     end
   end
 end
