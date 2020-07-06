e3ec557e11c2077ef98a3d181e5de9db835f5cfc
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-07-06 15:45:50 +0900

    Scheduleモデルのバリデーションメソッドのリファクタ

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -17,7 +17,7 @@ class Schedule < ApplicationRecord
   end
 
   def start_time_should_be_every_half_hour
-    if start_time.present? && (start_time.min != 0 || start_time.min != 30)
+    if start_time.present? && start_time.min % 30 != 0
       errors.add(:start_time, ': 開始時刻は毎時0分、または30分です')
     end
   end
@@ -25,7 +25,7 @@ class Schedule < ApplicationRecord
   def business_hour_on_weekday
     is_weekday = start_time.present? && start_time.wday % 6 != 0
 
-    if is_weekday && start_time.hour < 10 && start_time > 17
+    if is_weekday && (start_time.hour < 10 || 17 < start_time.hour)
       errors.add(:start_time, ': 平日の開始時刻は10:00~17:30内です')
     end
   end
@@ -33,7 +33,7 @@ class Schedule < ApplicationRecord
   def business_hour_on_saturday
     is_saturday = start_time.present? && start_time.wday == 6
 
-    if is_saturday && start_time.hour < 11 && start_time > 14
+    if is_saturday && (start_time.hour < 11 || 14 < start_time.hour)
       errors.add(:start_time, ': 土曜日の開始時刻は11:00~14:30内です')
     end
   end
