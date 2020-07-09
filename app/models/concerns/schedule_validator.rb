1f592ab2cf71ef0aaf3914b76be2cb0c68be08e0
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-07-09 15:14:57 +0900

    共通のガード節を各バリデーションメソッドの前に移動

--- app/models/concerns/schedule_validator.rb
+++ app/models/concerns/schedule_validator.rb
@@ -1,5 +1,7 @@
 class ScheduleValidator < ActiveModel::Validator
   def validate(record)
+    return if !record.start_time.present?
+
     start_time_cannot_be_in_the_past(record)
     start_time_should_be_every_half_hour(record)
     business_hour_on_weekday(record)
@@ -8,19 +10,19 @@ class ScheduleValidator < ActiveModel::Validator
   end
 
   def start_time_cannot_be_in_the_past(record)
-    if record.start_time.present? && record.start_time < DateTime.now
+    if record.start_time < DateTime.now
       record.errors.add(:start_time, ': 過去の日付を選択することは出来ません')
     end
   end
 
   def start_time_should_be_every_half_hour(record)
-    if record.start_time.present? && record.start_time.min % 30 != 0
+    if record.start_time.min % 30 != 0
       record.errors.add(:start_time, ': 開始時刻は毎時0分、または30分です')
     end
   end
 
   def business_hour_on_weekday(record)
-    is_weekday = record.start_time.present? && record.start_time.wday % 6 != 0
+    is_weekday = record.start_time.wday % 6 != 0
 
     if is_weekday && (record.start_time.hour < 10 || 17 < record.start_time.hour)
       record.errors.add(:start_time, ': 平日の開始時刻は10:00~17:30内です')
@@ -28,7 +30,7 @@ class ScheduleValidator < ActiveModel::Validator
   end
 
   def business_hour_on_saturday(record)
-    is_saturday = record.start_time.present? && record.start_time.wday == 6
+    is_saturday = record.start_time.wday == 6
 
     if is_saturday && (record.start_time.hour < 11 || 14 < record.start_time.hour)
       record.errors.add(:start_time, ': 土曜日の開始時刻は11:00~14:30内です')
@@ -36,7 +38,7 @@ class ScheduleValidator < ActiveModel::Validator
   end
 
   def close_on_sunday(record)
-    is_sunday = record.start_time.present? && record.start_time.wday == 0
+    is_sunday = record.start_time.wday == 0
 
     if is_sunday
       record.errors.add(:start_time, ': 日曜日は休業日です')
