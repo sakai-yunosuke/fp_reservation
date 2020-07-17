8c15f27bd01f89da80aea5003df341b13557d4f7
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-07-17 14:18:25 +0900

    存在性に関するバリデーション追加

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'presence' do
+      it { should belong_to(:user) }
+      it { should belong_to(:schedule) }
+    end
+  end
 end
