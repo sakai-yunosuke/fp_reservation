1332b4192ddd8fe465e7e66343ef9e895e102ed8
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-07-27 15:22:57 +0900

    同一ユーザーが同じ時間帯に複数の予約を持たないバリデーション追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,18 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
 
-  validates :schedule, presence: true, uniqueness: { scope: :user }
+  validate :user_cannot_reserve_at_the_same_time
+
+  def user_cannot_reserve_at_the_same_time
+    return if user.nil? || schedule.nil?
+
+    current_reservation = Reservation.find_by(user_id: user.id, schedule_id: schedule.id)
+    return if current_reservation.nil?
+
+    current_reservation_time = current_reservation.schedule.start_time
+
+    if Schedule.where(user_id: user.id, start_time: current_reservation_time).count >= 1
+      errors.add(:schedule, ': 同じ時間帯に他の予約が入っています')
+    end
+  end
 end
