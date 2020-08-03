14f118bcc50bcc4f52030e0c7412614e4fa9bc6f
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-08-03 15:23:05 +0900

    Reservationモデル内でstart_timeにdelegateを使用

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,15 +1,14 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
+  delegate :start_time, to: :schedule
 
   validate :user_cannot_reserve_at_the_same_time
 
   def user_cannot_reserve_at_the_same_time
     return if user.nil? || schedule.nil?
 
-    current_reservation_time = schedule.start_time
-
-    if Schedule.joins(:reservation).where(start_time: current_reservation_time).merge(Reservation.where(user_id: user.id)).count >= 1
+    if Schedule.joins(:reservation).where(start_time: start_time).merge(Reservation.where(user_id: user.id)).count >= 1
       errors.add(:schedule, ': 同じ時間帯に他の予約が入っています')
     end
   end
