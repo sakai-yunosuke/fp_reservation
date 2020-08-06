75e65924d5e31ad93ba8ebcf2f7e4b76f25c81cb
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-08-06 15:23:04 +0900

    バリデーションのリファクタ

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,7 +8,7 @@ class Reservation < ApplicationRecord
   def user_cannot_reserve_at_the_same_time
     return if user.nil? || schedule.nil?
 
-    if Schedule.joins(:reservation).where(start_time: start_time).merge(Reservation.where(user_id: user.id)).count >= 1
+    if Reservation.joins(:schedule).where(user: user).merge(Schedule.where(start_time: start_time)).count >= 1
       errors.add(:schedule, ': 同じ時間帯に他の予約が入っています')
     end
   end
