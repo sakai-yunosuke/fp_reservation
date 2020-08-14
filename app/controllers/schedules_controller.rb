415f15e664977671890d80364d631f71f294975d
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-08-14 16:32:38 +0900

    scheduleのcreateアクション実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,21 @@
 class SchedulesController < ApplicationController
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      flash[:success] = 'スケジュールが登録されました'
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  
+  def schedule_params
+    params.require(:schedule).permit!
+  end
 end
