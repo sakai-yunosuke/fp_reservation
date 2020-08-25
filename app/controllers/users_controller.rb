7cfd903265a4c3dd8ce9bc43bf8f1e5fc4b29993
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-08-25 16:05:16 +0900

    ユーザー詳細ページのカレンダーで予約した枠と予約された受付枠を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @schedules = Schedule.where(user: @user)
+    @reservations = Reservation.where(user: @user)
   end
 
   private
