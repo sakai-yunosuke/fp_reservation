5cdba221f8b237aa524dc583801a9d95f20fd5c9
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-08-26 16:25:34 +0900

    予約失敗時にエラーメッセージを表示するよう変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,13 @@
 class ReservationsController < ApplicationController
   def create
-    @reservation = current_user.reservations.build(reservation_params)
-    if @reservation.save
+    begin
+      current_user.reservations.create!(reservation_params)
+      
       flash[:success] = '予約が完了しました'
       redirect_to current_user
-    else
-      # TODO: 異常系の処理
+    rescue ActiveRecord::RecordInvalid => e
+      flash[:danger] = '予約に失敗しました、もう一度お試しください'
+      redirect_to schedules_path
     end
   end
 
