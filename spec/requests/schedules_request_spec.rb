081a5fbb71327427c2d64724c48106b8415a5d0d
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-09-17 17:40:16 +0900

    スケジュール作成画面と検索画面へのアクセス時のテストを追加

--- spec/requests/schedules_request_spec.rb
+++ spec/requests/schedules_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Schedules", type: :request do
+RSpec.describe SchedulesController, type: :request do
+  describe 'GET /schedules' do
+    context 'when not logged in' do
+      it { is_expected.to eq(302) }
+    end
 
+    context 'when logged in' do
+      let(:user) { create(:user) }
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: user.id)
+      end
+
+      it { is_expected.to eq(200) }
+    end
+  end
+
+  describe 'GET /schedules/new' do
+    context 'when not logged in' do
+      it { is_expected.to eq(302) }
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:user) }
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: user.id)
+      end
+
+      it { is_expected.to eq(200) }
+    end
+  end
 end
