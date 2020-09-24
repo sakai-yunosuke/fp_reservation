93f56fa62f6dfacae22227fe1407d09e5e3f9930
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-09-24 17:12:29 +0900

    ScheduleControllerについてscheduleの作成に関するspecを追加

--- spec/requests/schedules_request_spec.rb
+++ spec/requests/schedules_request_spec.rb
@@ -12,6 +12,44 @@ RSpec.describe SchedulesController, type: :request do
     end
   end
 
+  describe 'POST /schedules' do
+    context 'when not logged in' do
+      it 'should be fail and redirect to home' do
+        is_expected.to eq 302
+        expect(flash[:danger]).to eq 'ログインが必要です'
+      end
+    end
+
+    context 'when logged in' do
+      include_context 'when login required'
+      let(:schedule){ FactoryBot.create(:schedule) }
+
+      context 'with invalid input' do
+        before do
+          params[:schedule] = FactoryBot.attributes_for(:schedule, :in_the_past)
+        end
+
+        it 'should be fail and redirect to new schedule page' do
+          expect{subject}.to_not change(Schedule, :count)
+          is_expected.to eq 302
+          expect(flash[:danger]).to eq 'スケジュールの作成に失敗しました、もう一度お試しください'
+        end
+      end
+
+      context 'with valid input' do
+        before do
+          params[:schedule] = FactoryBot.attributes_for(:schedule)
+        end
+
+        it 'should be success and redirect to user page' do
+          expect{subject}.to change(Schedule, :count).by(1)
+          is_expected.to eq 302
+          expect(flash[:success]).to eq 'スケジュールが登録されました'
+        end
+      end
+    end
+  end
+
   describe 'GET /schedules/new' do
     context 'when not logged in' do
       it { is_expected.to eq(302) }
